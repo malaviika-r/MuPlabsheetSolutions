c523d77c77342fbea46416bae7e18fe990099b50
malaviika-r <167843109+malaviika-r@users.noreply.github.com>
2024-04-23 15:28:51 +0530

    Create Lab4-Task1.asm

---
+++ Lab4-Task1.asm
@@ -0,0 +1,26 @@
+.model tiny
+.data
+			`          dat1 		    db 'wasitcatisaw'
+			           dat1length   db 0bh
+			           res 		      dw 00h
+.code
+.startup
+			    lea si, dat1
+		    	lea di, dat1
+			    mov cx, 00h
+			    mov cl, dat1length
+			    add di, cx
+			    dec di
+continue:	mov bl, [di]
+    			cmp [si], bl
+		    	jne fin
+			    mov dx, 01h
+			    dec di
+			    inc si
+			    dec cx
+			    jnz continue
+			    jz last			
+fin:		  mov dx, 00h
+last: 	  mov res, dx
+.exit
+end
