592e42d426083745d8902d74b5e4ce48c55cb4a3
malaviika-r <167843109+malaviika-r@users.noreply.github.com>
2024-04-23 14:34:18 +0530

    Update Lab3-Task1.asm

--- Lab3-Task1.asm
+++ Lab3-Task1.asm
@@ -4,9 +4,9 @@
 .code
 .startup
 		lea bx, array1
-		mov cl, 0Ah 		// count 
-		mov al, 0Ah		// to check for the presence of "0A"
-		mov DX, 45h 		// ASCII code for "E" is 45h
+		mov cl, 0Ah 		; count 
+		mov al, 0Ah		; to check for the presence of "0A"
+		mov DX, 45h 		; ASCII code for "E" is 45h
 	X1: 	cmp al, [bx]
 		jne X2
 		mov [bx], DX
