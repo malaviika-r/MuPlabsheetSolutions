ca919936ec7acef20cb4a2da5b1514d13d5dda2a
Malavika <167843109+malaviika-r@users.noreply.github.com>
2024-04-24 03:19:11 +0530

    Update Lab4-Task2.asm

--- Lab4-Task2.asm
+++ Lab4-Task2.asm
@@ -1,32 +1,32 @@
 .model tiny
 .data
-		string 			db 'BITSIOTLAB'
-		substring 		db 'IOT'
+		string 		db 'BITSIOTLAB'
+		substring 	db 'IOT'
 		stringlength 	db 0Ah
 		substringlength db 03h
 .code
 .startup
-				lea bx, string 
-				mov dl, stringlength
+		lea bx, string 
+		mov dl, stringlength
 compareString:  mov si, bx
-				lea di, substring
-				mov cl, substringlength
-				repe cmpsb
-				cmp cl, 00h
-				jz substringFound
-				inc bx
-				dec dl
-				jnz compareString
-				jmp end_prog
+		lea di, substring
+		mov cl, substringlength
+		repe cmpsb
+		cmp cl, 00h
+		jz substringFound
+		inc bx
+		dec dl
+		jnz compareString
+		jmp end_prog
 substringFound: mov al, '*'
-				mov [bx], al
-				mov di, bx
-				inc di
-				mov cl, dl
-				sub cl, substringlength
+		mov [bx], al
+		mov di, bx
+		inc di
+		mov cl, dl
+		sub cl, substringlength
 repeatCopying:  lodsb
-				stosb
-				loop repeatCopying
+		stosb
+		loop repeatCopying
 end_prog:       nop
 .exit 
 end
